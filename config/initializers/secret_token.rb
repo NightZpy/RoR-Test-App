b4cd1294e9cd9df6f757d2acd7956fde45aa139a
Lenyn Alcántara <nightzpy@gmail.com>
2012-05-30 22:08:18 -0430

    Primer commit aplicación con Product Generate Ready

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'b4e643716bbad7c546c054d1d9f0729d94d9f111117a44b9460a3ac49aef6dc72d1b04bb8e3347ed26a1e577a01393cb68630d637a34abb128549ae2fd2923fe'
