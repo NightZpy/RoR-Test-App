b5ab3609987bdb232571c0685f8bd30bfb092e30
Lenyn Alcántara <nightzpy@gmail.com>
2012-05-31 03:11:24 -0430

    Agregado nuevo controlador (store) y ubicado como home del sitio web, routes.rb configurado para ello

--- db/migrate/20120530213915_create_products.rb
+++ db/migrate/20120530213915_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration
   def self.up
     create_table :products do |t|
-      t.string :title
+      t.string :title, :length => 8
       t.text :description
       t.string :image_url
-      t.decimal :price, :precision => 15, :scale => 2
+      t.decimal :price, :precision => 8, :scale => 2
 
       t.timestamps
     end
