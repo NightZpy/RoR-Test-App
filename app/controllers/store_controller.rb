2d0de26ddba4e965040600d8f83c8ddd4643d24f
Lenyn Alcántara <nightzpy@gmail.com>
2012-06-01 05:59:10 -0430

    Agregados productos seleccionados a un carrito de compras, se muestra la lista de productos.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.all
+  	@time = Time.now
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120530213915) do
+ActiveRecord::Schema.define(:version => 20120531103952) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", :force => true do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", :force => true do |t|
-    t.string   "title", :length => 8
+    t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price",       :precision => 8, :scale => 2
+    t.decimal  "price",       :precision => 15, :scale => 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
