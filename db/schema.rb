5b9da8528b41c788b54750755bfa5eea1c8627d3
Lenyn Alcántara <nightzpy@gmail.com>
2012-06-11 03:45:34 -0430

    Arregladas pruebas funcionales para line_item y agregadas pruebas funcionales para el Ajax del Store

--- db/schema.rb
+++ db/schema.rb
@@ -1,11 +1,3 @@
-#---
-# Excerpted from "Agile Web Development with Rails",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
-#---
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -19,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110711000005) do
+ActiveRecord::Schema.define(:version => 20120530213915) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
