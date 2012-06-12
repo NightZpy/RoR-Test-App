907c3e524dbb6124f1ed3661cf504aa5a9da906b
Lenyn Alcántara <nightzpy@gmail.com>
2012-06-12 13:31:48 -0430

    Agregadas opciones para login de admin y acceso controlado, además de las pruebas correspondientes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery
+  before_filter :authorize
   #skip_before_filter :verify_authenticity_token, :only => [:my_form_action]
   private
 
@@ -18,5 +19,13 @@ class ApplicationController < ActionController::Base
       cart = Cart.create
       session[:cart_id] = cart.id
       cart
+    end
+
+  protected
+
+    def authorize
+      unless User.find_by_id(session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end      
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,16 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 Depot::Application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  resources :users
+
   resources :orders
 
   resources :line_items
@@ -16,7 +26,7 @@ Depot::Application.routes.draw do
   get "store/index"
 
   resources :products do
-    get :who_bought, on: :member    
+    get :who_bought, on: :member
   end
 
   # ...
